bc58aae2d52e0c74e0576d496977b3b46a94dac0
Bryan Odongo <152975669+bryan-odongo@users.noreply.github.com>
2025-02-02 16:08:05 +0300

    Update v1 task_4.sql

--- task_4.sql
+++ task_4.sql
@@ -1,7 +1,7 @@
 -- Use the alx_book_store database
 USE alx_book_store;
 
--- Retrieve the full description of the books table from information_schema
+-- Retrieve the full description of the Books table from information_schema
 SELECT COLUMN_NAME, COLUMN_TYPE, IS_NULLABLE, COLUMN_KEY, COLUMN_DEFAULT, EXTRA
 FROM INFORMATION_SCHEMA.COLUMNS
-WHERE TABLE_SCHEMA = 'alx_book_store' AND TABLE_NAME = 'books';
+WHERE TABLE_SCHEMA = 'alx_book_store' AND TABLE_NAME = 'Books';
